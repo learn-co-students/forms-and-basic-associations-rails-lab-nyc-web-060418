82fce9b4db0f80684ba04bd9fe3fe1d2cd0080c2
DevinPierce <devinmpierce@gmail.com>
2018-07-09 20:46:49 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,7 +13,7 @@ class SongsController < ApplicationController
 
   def create
     @song = Song.new(song_params)
-
+    @song.note_contents = params[:song][:notes]
     if @song.save
       redirect_to @song
     else
@@ -47,7 +47,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :song_genre_id, :notes)
   end
 end
-
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,37 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def song_genre_id=(id)
+    self.genre = Genre.find(id)
+  end
+
+  def song_genre_id
+    self.genre ? self.genre.id : nil
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def note_contents=(notes)
+    notes.each {|n| self.notes << Note.create(content: n) unless n == ""}
+  end
+
+  def note_contents
+    self.notes.map {|n| n.content}
+  end
+
+end
